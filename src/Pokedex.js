e06c03f2e3d5dd524b092c334fe9254e02e6621c
Abdelaziz <abdelazizMalfy@gmail.com>
2019-08-16 19:40:15 +0200

    Pokemon Card added

---
+++ src/Pokedex.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import './Pokedex.css'
+
+import PokeCard from './PokeCard'
+
+class Pokedex extends Component {
+    static defaultProps = {
+        pokemons: [
+            {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
+            {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
+            {id: 11, name: 'Matapod', type: 'bug', base_experience: 72},
+            {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
+            {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
+            {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
+            {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+            {id: 133, name: 'Eevee', type: 'norma', base_experience: 65},
+        ]
+    }
+
+    render() {
+        
+        return (
+            <div className="Pokedex">
+                <h1>Pokedex!</h1>
+                <div className="Pokedex-cards">
+                    {this.props.pokemons.map((pokemon) => (
+                        <PokeCard 
+                        id={pokemon.id} 
+                        name={pokemon.name}
+                        type={pokemon.type}
+                        exp={pokemon.base_experience}
+                        />
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Pokedex
